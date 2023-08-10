23c3d7ce89620209eaf54124b23cb417f8281bf2
Sanjay <ksanjayk@protonmail.com>
2023-08-10 16:45:13 +0530

    validation pipes added for create Task

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // whenever a validation decorator is encountered
+  // the below line specifies to run the validation instead of asking each controller
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
