ad9f767fb7cca14d06d39751433056c49e3f7c9f
Sanjay <ksanjayk@protonmail.com>
2023-08-18 13:04:24 +0530

    unique username and errorhandling

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,6 +1,10 @@
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AppDataSource } from 'src';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 export const customUsersRepository = AppDataSource.getRepository(User).extend({
   async createUser(authCredentialsDto: AuthCredentialsDto) {
@@ -9,6 +13,14 @@ export const customUsersRepository = AppDataSource.getRepository(User).extend({
       username,
       password,
     });
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code == 23505) {
+        throw new ConflictException(`Username already exits`);
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   },
 });
