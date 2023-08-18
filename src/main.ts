92a8df186a1aa7d0e863d3a0687a83a70a29e031
Sanjay <ksanjayk@protonmail.com>
2023-08-18 19:51:16 +0530

    serialization of data

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // whenever a validation decorator is encountered
   // the below line specifies to run the validation instead of asking each controller
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 }
 bootstrap();
