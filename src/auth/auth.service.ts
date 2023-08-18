035f8e61c9464e76736ff3fb86a28d2adf3fce71
Sanjay <ksanjayk@protonmail.com>
2023-08-18 18:10:57 +0530

    jwt generation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,17 +2,25 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { customUsersRepository } from './users.repository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload-interface';
 
 @Injectable()
 export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
   signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return customUsersRepository.createUser(authCredentialsDto);
   }
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto;
     const user = await customUsersRepository.findOne({ where: { username } });
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'success';
+      const payload: JwtPayload = { username };
+      const accessToken = await this.jwtService.sign(payload);
+      return { accessToken };
     } else {
       throw new UnauthorizedException('Please check your login credentials');
     }
